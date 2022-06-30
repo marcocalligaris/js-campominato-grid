e43c41d138dad692c2fc09773fef0ae1e65f25eb
marcocalligaris <marcocally@gmail.com>
2022-06-30 17:40:06 +0200

    Milestone 4

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-// * Funzioni
 function createNewCell(content) {
     const newcell = document.createElement('div');
     newcell.classList.add('cell');
@@ -11,22 +5,23 @@ function createNewCell(content) {
     return newcell;
 }
 
-
-// * Recupero dal DOM gli elementi che mi servono
 const grid = document.getElementById('grid');
 const button = document.getElementById('play-btn');
-
-
-// * Imposto righe e celle
 const rows = 10;
 const cells = 10;
 const gridCells = rows * cells;
 
-// * Al click sul bottone, si genera la griglia
 button.addEventListener('click', function() {
     for(let i = 1; i <= gridCells; i++) {
         const cell = createNewCell(i);
         grid.appendChild(cell);
-        
-    }
+
+        cell.addEventListener('click', function () {
+            console.log('Hai cliccato la cella numero ' + [i])
+            cell.classList.toggle('clicked');
+        })
+    } 
 })
+
+
+
